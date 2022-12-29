6e62fce1b61e1ea72cb8aaf2b8465694fef7a79a
raketej <jennifer.rakete@exxeta.com>
2022-12-29 17:40:19 +0100

    first commit

---
+++ src/app/components/items-banner/items-banner.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { Item } from '../item/item';
+
+@Component({
+  selector: 'items-banner',
+  templateUrl: './items-banner.component.html',
+  styleUrls: ['./items-banner.component.scss']
+})
+export class ItemsBannerComponent {
+  @Input() items: Item[] = [];
+  @Input() title: string = '';
+}
